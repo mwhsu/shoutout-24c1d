9c76ac810ab7f5ae20ccfbb36456b0137cf707e4
Stackbit Code Editor <projects@stackbit.com>
2021-04-01 23:11:45 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,20 +20,6 @@ class MyDocument extends Document {
                     <ScriptTag src={withPrefix('js/page-load.js')}/>
                     <ScriptTag src={withPrefix('js/page-unload.js')}/>
                     <NextScript />
-                    /** Start of Tawk.to Script **/
-                        <script type="text/javascript">
-                        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-                        (function(){
-                            s1=document.createElement("script");
-                            s0=document.getElementsByTagName("script")[0];
-                            s1.async=true;
-                            s1.src='https://embed.tawk.to/60664d0f067c2605c0be90f5/1f27pq0tu';
-                            s1.charset='UTF-8';
-                            s1.setAttribute('crossorigin','*');
-                            s0.parentNode.insertBefore(s1,s0);
-                        })();
-                        </script>
-                    /** End of Tawk.to Script **/
                 </body>
             </Html>
         );
