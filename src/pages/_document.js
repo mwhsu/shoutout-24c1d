fa859f437fddd79c1558f5f4185953189747f02b
Stackbit Code Editor <projects@stackbit.com>
2021-04-01 23:13:22 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -20,6 +20,7 @@ class MyDocument extends Document {
                     <ScriptTag src={withPrefix('js/page-load.js')}/>
                     <ScriptTag src={withPrefix('js/page-unload.js')}/>
                     <NextScript />
+                    <ScriptTag src={withPrefix('js/page-unload.js')}/>
                 </body>
             </Html>
         );
