783d30b5936f76e1b028da7e2e5b54fc68703b78
Stackbit Code Editor <projects@stackbit.com>
2021-04-02 19:12:20 +0000

    _document.js: changed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,7 +13,17 @@ class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head></Head>
+                <Head>
+                    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-FY7XJ61B78"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-FY7XJ61B78');
+</script>
+                </Head>
                 <body>
                     <Main />
                     <ScriptTag src={withPrefix('js/init.js')}/>
